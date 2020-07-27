70e3d562b7828765d6dca69c3b9beeebc361394c
Danjiri <39514453+Danjiri@users.noreply.github.com>
2020-07-27 15:48:56 +0900

    Update kanji_aggregation.py

--- kanji_aggregation.py
+++ kanji_aggregation.py
@@ -4,17 +4,40 @@ import csv
 
 def split_fullname(filepath):
 
-    quote = '"'
-    filepath = quote + filepath + quote
+
     kanji_data = pd.read_csv(filepath, header=None)
     kanji_array = np.array
     kanji_array = kanji_data.values
+    print(kanji_array)
+    count = kanji_array.shape[0]
+    print(kanji_data)
+
+    for i in range(count):
+        fullname = kanji_array[i]
+        print(fullname)
+        position = np.where([fullname == " "])
+        print(position)
+        surname = [] 
+        surname.append(fullname[:position])
+        given_name = []
+        surname.append(fullname[len(fullname) - position:])
+        print(surname)
+        print(given_name)
+
+        
+
+
+#    kanji_array.find(' ') #半角
+
+#    [0][]
+
+
 
 def analyze_names(parameter_list):
-    pass
+    a = 0
 
 def insert(parameter_list):
-    pass
+    b = 0
 
 
 
